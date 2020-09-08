7ddd5ef1f9d7012856e83d31f57a7c56c92099b3
Andrew3232 <oleinitch@gmail.com>
2020-09-08 22:33:30 +0300

    music site
    download of work done

---
+++ js/ajax_content.js
@@ -0,0 +1,56 @@
+var lang = "ru";
+	function UploadContent(){
+	//if(lang = "ru"){
+  		$(document).ready(function(){
+  			//header
+   			 $('#id_1').load('content/ru/content.html #menu_1');
+   			  $('#id_2').load('content/ru/content.html #menu_2');
+   			   $('#id_3').load('content/ru/content.html #menu_3');
+   			    $('#id_4').load('content/ru/content.html #menu_4');
+   			     $('#id_5').load('content/ru/content.html #menu_5');
+   			      $('#id_6').load('content/ru/content.html #menu_6');
+   			       $('#id_7').load('content/ru/content.html #menu_7');
+   			       //top page
+   			       $('#top_pag_con_2').load('content/ru/content.html #top_pag_2');
+   			       	$('#top_pag_con_4').load('content/ru/content.html #top_pag_4');
+   			       	 $('#top_pag_con_5').load('content/ru/content.html #top_pag_5');
+   			          $('#top_pag_con_6').load('content/ru/content.html #top_pag_6');
+   			           $('#top_pag_con_7').load('content/ru/content.html #top_pag_7');
+   			           // slider
+   			           $('#Wlcme').load('content/ru/content.html #Welcome');
+   			       		$('#Slidf').load('content/ru/content.html #Slider2');
+   			     	  	 $('#Slidttr').load('content/ru/content.html #Slider3');
+
+   			     	  	 $('#WELCOMES_W').load('content/ru/content.html #WelcomeS');
+   			     	  	 	//singer
+				       		  $('#Top_singers').load('content/ru/content.html #Top_singer');
+				       	 	   $('#Img_drgn_h').load('content/ru/content.html #Imagine_hov');
+				         		$('#Img_drgn_txt').load('content/ru/content.html #Imagine_text');
+				           		 $('#Miyagi_h').load('content/ru/content.html #Miyagi_hov');
+				           		  $('#Miyagi_t').load('content/ru/content.html #Miyagi_text');
+				           		   $('#Twenty_one_h').load('content/ru/content.html #Twenty_one_hov');
+				           		    $('#Twenty_one_txt').load('content/ru/content.html #Twenty_one_text');
+				           		     $('#Inna_h').load('content/ru/content.html #Inna_hov');
+				           		  	  $('#Inna_txt').load('content/ru/content.html #Inna_text');
+				           		//last_news  	 
+				           		$('#Lst_News').load('content/ru/content.html #Last_New');
+				           		  $('#dt_news').load('content/ru/content.html #Date_New');
+				           		    $('#twenty_tour').load('content/ru/content.html #Twenty_Tour');				           		      
+				           		  	  $('#News_txt').load('content/ru/content.html #New_text'); 
+				           		  	  //End page_1 	      
+				           		  	  				//page_2
+
+
+	           	// footer
+	           $('#foot_Quote').load('content/ru/content.html #foot_Q');
+	            $('#foot_Author').load('content/ru/content.html #foot_A');
+	             $('#Conn_foot').load('content/ru/content.html #foot_C');
+	              $('#Copyri_foot').load('content/ru/content.html #Copyri_f');
+	               $('#creater_f').load('content/ru/content.html #creater');
+
+  		});
+	
+  	}
+
+
+
